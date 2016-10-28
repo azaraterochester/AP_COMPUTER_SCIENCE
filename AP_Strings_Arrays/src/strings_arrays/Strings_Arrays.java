1c1f265a5cdf94b456170f8f4bb28f5abaafd245
azarateo <azarate@cool4code.com>
2016-10-28 12:42:40 -0500

    Examples of string methods And Arrays

--- AP_Strings_Arrays/src/strings_arrays/Strings_Arrays.java
+++ AP_Strings_Arrays/src/strings_arrays/Strings_Arrays.java
@@ -17,24 +17,38 @@ public class Strings_Arrays {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //------------------------- STRINGS ------------------------
+
         //Create a String
-        String myString = "Hello";
+        String myString = "Alejo$";      
+        System.out.println("My string is: "+myString);
         
-        //Create a String array
-        String[] arrayOfStrings = {"Juan","María","Ana","Andres","Juan#"};
+        //replace # for a $
+        String modifiedString = myString.replace("$","#");       
+        System.out.println("Modified string: "+modifiedString);
+        
+        //use first 5 letters of the string
+        String first4Letters = modifiedString.substring(0,5);
+        System.out.println("First four letters: "+first4Letters);
+
+        //-------------------- ARRYS -----------------------------
+        
+        //Create a String array 
+        String[] arrayOfStrings = {"Juan","María","Ana","Andres","--"};
+        
+         //Length of an array
+        int howManyElementsInTheArray = arrayOfStrings.length;
+        System.out.println("\nNumber of elements in the array: "+howManyElementsInTheArray);
         
         //Print out an element of a String Array
-        System.out.println(arrayOfStrings[0]);
+        System.out.println("Element 0 of the array: "+arrayOfStrings[0]);
         
-        //Length of an array
-        int howManyElementsInTheArray = arrayOfStrings.length;
-        System.out.println(howManyElementsInTheArray);
-        
-        myString = arrayOfStrings[arrayOfStrings.length];
-        
-        
-        
-        
+        //Last element of the array
+        System.out.println("Last element 0 of the array: "+arrayOfStrings[howManyElementsInTheArray-1]);
+        //Change last element
+        arrayOfStrings[howManyElementsInTheArray-1] = first4Letters;
+        System.out.println("Last element 0 of the array: "+arrayOfStrings[howManyElementsInTheArray-1]);
+                
     }
     
 }
