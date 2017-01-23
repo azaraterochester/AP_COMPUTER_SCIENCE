e1767a9ff654207a4edcb1818d5728f74d81f5fd
azaraterochester <azarate@rochester.edu.co>
2017-01-23 11:45:05 -0500

    AP_Computer_Science
    Class example. Student Story

--- studentClass/src/story/MainStory.java
+++ studentClass/src/story/MainStory.java
@@ -1,47 +1,31 @@
 package story;
-import java.util.Date;
-
 public class MainStory {
-
     public static void main(String[] args) {
-    
         System.out.println("Once upon a time there was an empty kingdom");
         System.out.println("In this kingdom there was only one God. Me.");
-        System.out.println("One day I decided to create a character. He was going to be a student");
-        
-        
+        System.out.println("One day I decided to create a character. He was going to be a student");   
         //And the gos of the empty kingdom created Neo
         System.out.println("God creates Neo");
-        
         //Instantiate a new object of type Student. This instance is called neo
-        Student neo = new Student();
-        
+        Student bello = new Student();
+        Student andres = new Student();
         //Initialize neo's name and lastname attributes
-        neo.name = "Sergio";
-        neo.lastname = "Guerra";
-        
+        bello.name = "Sergio";
+        bello.lastName = "Guerra";
         //God was good and taught neo to say one word
         System.out.println("God teaches Neo a sound");
-        neo.sound = "Pikachu";
-        
-        
+        bello.sound = "sergio capa y a rojas le gusta castro";
         //And God ordererd neo to sing and say his name
         System.out.println("God orders Neo to sing and say his name");
-        
         //And neo sings 
         System.out.println("And Neo says his name and sings");
-       
         //neo says his name
-        System.out.println(neo.sayYourName());
+        System.out.println(bello.sayYourName());
         //neo sings
-        neo.sing();
-        
+        bello.sing();
         //And god thinks 
-        System.out.println("Mucha aleta");
-        
-        
-        
-    
-    }
-    
+        bello.sing(10);
+        bello.sound = "soy loca ademas de gay y a cali lo cascan en la ruta";
+        bello.sing(7);
+    }   
 }
--- studentClass/src/story/Student.java
+++ studentClass/src/story/Student.java
@@ -1,26 +1,25 @@
 package story;
-
-public class Student {
-    
-    
-    //God created a student with
+public class Student {   
+   
     public String name;
-    public String lastname;
+    public String lastName;
     public String course;
     public String sound;
     
+    public void sing(int cuantasVeces){
+        for(int a = 0; a < cuantasVeces; a++){
+            System.out.println(this.sound);
+        }
+    }
     
-    //God gave every student the ability to sing by repeating the sound he could do four times
     public void sing(){
-        
-        String song = "-- I like to sing: "+sound + " " + sound + " " + sound + " " + sound + " ";
+        String song = "-- I like to sing: "+sound + " " + sound + " " + sound + " ";
         System.out.println(song);
     }
-    //God gave every student the ability to say their name
+    
     public String sayYourName(){
-        String name = "-- My name is "+this.name +" "+ this.lastname;
-        return name;
+        String message = "-- My name is "+this.name +" "+ this.lastName;
+        return message;
     }
-    
-    
+
 }
